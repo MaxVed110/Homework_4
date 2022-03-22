38aa43180c0bd2f04fa3b1c48548a1b8e070bf30
Maxim <vedernikovmaxim@rambler.ru>
2022-03-22 19:56:51 +0300

    Решена задача 25

---
+++ Зад_25_А_в_степени_В/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в 
+натуральную степень B.
+3, 5 -> 243 (3⁵)
+*/
+
+double NumberPow(int numberA, int numberB)
+{
+   double result = Math.Pow(numberA, numberB);
+   return result;
+}
+
+Console.WriteLine("Введите 2 числа: ");
+
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+int TwoNumber = Convert.ToInt32(Console.ReadLine());
+
+double resultPow =  NumberPow(FirstNumber, TwoNumber);
+
+Console.WriteLine($"Число {FirstNumber} в степени {TwoNumber}  = {resultPow}");
