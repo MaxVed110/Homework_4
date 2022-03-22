b07aa2245101034211a3714858f0c90b6615431d
Maxim <vedernikovmaxim@rambler.ru>
2022-03-22 20:09:58 +0300

    Решена задача 27

---
+++ Зад_27_сумма_цифр_в_числе/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+*/
+
+int SumNumber(int number)
+{
+    int sum = 0;
+    if (number >= 0)
+    {
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+    }
+    else
+    {
+        number *= (-1);
+        while (number > 0)
+        {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+int sum = SumNumber(UserNumber);
+
+Console.WriteLine($"Сумма цифр в числе {UserNumber} = {sum}");
